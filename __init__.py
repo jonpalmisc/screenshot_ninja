633407106dfcc76112739ed8ce2e90927240d5bb
Jon Palmisciano <jp@jonpalmisc.com>
2021-03-25 15:48:32 -0400

    Cleaned up duplicated code

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ from binaryninjaui import DockHandler
 
 from PySide6.QtCore import QPoint, QRect, QSize
 from PySide6.QtGui import QPixmap, QRegion
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget
 
 
 def scaled_size(r: QRect, scale: int) -> QSize:
@@ -26,27 +26,19 @@ def scaled_size(r: QRect, scale: int) -> QSize:
     return s.size()
 
 
-def find_main_window() -> Optional[QMainWindow]:
-    """Find the application's main window."""
+def _save_widget_image(w: QWidget, scale: int) -> None:
+    """
+    Save an image of the given widget.
 
-    a = QApplication.instance()
+    :param scale: the scaling factor for the image
+    """
 
-    for w in a.topLevelWidgets():
-        if isinstance(w, QMainWindow):
-            return w
-
-    return None
-
-
-def _save_window_image(scale: int) -> None:
-    mw = find_main_window()
-
-    vf_rect = mw.rect()
+    r = w.rect()
 
     # Create a QPixmap and render the view widget to it
-    screenshot = QPixmap(scaled_size(vf_rect, scale))
-    screenshot.setDevicePixelRatio(scale)
-    mw.render(screenshot, QPoint(), QRegion(vf_rect))
+    img = QPixmap(scaled_size(r, scale))
+    img.setDevicePixelRatio(scale)
+    w.render(img, QPoint(), QRegion(r))
 
     # Use "binaryninja-" + the current Unix time as the default filename
     default_filename = f"binaryninja-{int(time.time())}.png"
@@ -58,12 +50,38 @@ def _save_window_image(scale: int) -> None:
         return
 
     # Attempt to save the screenshot and show an error message if unsuccessful
-    if screenshot.save(save_path.decode("ascii")):
+    if img.save(save_path.decode("ascii")):
         print(f"Screenshot saved to {save_path.decode('ascii')} successfully.")
     else:
         show_message_box(
             "Error",
-            "Failed to save screenshot",
+            "Failed to save screenshot.",
+            MessageBoxButtonSet.OKButtonSet,
+            MessageBoxIcon.ErrorIcon,
+        )
+    
+
+def _save_window_image(scale: int) -> None:
+    """
+    Save an image of the main window.
+
+    :param scale: the scaling factor for the image
+    """
+
+    app = QApplication.instance()
+    main_window = None
+
+    for w in app.topLevelWidgets():
+        if isinstance(w, QMainWindow):
+            main_window = w
+
+    # Save an image of the main window if found
+    if main_window is not None:
+        _save_widget_image(main_window, scale)
+    else:
+        show_message_box(
+            "Error",
+            "Couldn't find main window.",
             MessageBoxButtonSet.OKButtonSet,
             MessageBoxIcon.ErrorIcon,
         )
@@ -78,35 +96,12 @@ def _save_view_image(scale: int) -> None:
 
     dh = DockHandler.getActiveDockHandler()
 
-    # Get the current ViewFrame, the underlying widget, and that widget's rect
+    # Get the current ViewFrame and the underlying widget
     vf = dh.getViewFrame()
     vf_widget = vf.getCurrentWidget()
-    vf_rect = vf_widget.rect()
 
-    # Create a QPixmap and render the view widget to it
-    screenshot = QPixmap(scaled_size(vf_rect, scale))
-    screenshot.setDevicePixelRatio(scale)
-    vf_widget.render(screenshot, QPoint(), QRegion(vf_rect))
-
-    # Use "binaryninja-" + the current Unix time as the default filename
-    default_filename = f"binaryninja-{int(time.time())}.png"
-
-    # Ask for the user's preferred output path; if the output path is None, then
-    # the user has cancelled the operation
-    save_path = get_save_filename_input("Save Screenshot", "png", default_filename)
-    if save_path is None:
-        return
-
-    # Attempt to save the screenshot and show an error message if unsuccessful
-    if screenshot.save(save_path.decode("ascii")):
-        print(f"Screenshot saved to {save_path.decode('ascii')} successfully.")
-    else:
-        show_message_box(
-            "Error",
-            "Failed to save screenshot",
-            MessageBoxButtonSet.OKButtonSet,
-            MessageBoxIcon.ErrorIcon,
-        )
+    # Save an image of the active view
+    _save_widget_image(vf_widget, scale)
 
 
 def save_view_image_x1(_bv: BinaryView) -> None:

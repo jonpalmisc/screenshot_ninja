7125108e8d61801b846ea5d8028cd1b0599cca73
Jon Palmisciano <jp@jonpalmisc.com>
2021-03-25 15:30:39 -0400

    Added support for full-window screenshots

--- __init__.py
+++ __init__.py
@@ -6,6 +6,7 @@ from binaryninjaui import DockHandler
 
 from PySide6.QtCore import QPoint, QRect, QSize
 from PySide6.QtGui import QPixmap, QRegion
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 
 def scaled_size(r: QRect, scale: int) -> QSize:
@@ -23,6 +24,49 @@ def scaled_size(r: QRect, scale: int) -> QSize:
     s.setHeight(int(r.height() * scale))
 
     return s.size()
+
+
+def find_main_window() -> Optional[QMainWindow]:
+    """Find the application's main window."""
+
+    a = QApplication.instance()
+
+    for w in a.topLevelWidgets():
+        if isinstance(w, QMainWindow):
+            return w
+
+    return None
+
+
+def _save_window_image(scale: int) -> None:
+    mw = find_main_window()
+
+    vf_rect = mw.rect()
+
+    # Create a QPixmap and render the view widget to it
+    screenshot = QPixmap(scaled_size(vf_rect, scale))
+    screenshot.setDevicePixelRatio(scale)
+    mw.render(screenshot, QPoint(), QRegion(vf_rect))
+
+    # Use "binaryninja-" + the current Unix time as the default filename
+    default_filename = f"binaryninja-{int(time.time())}.png"
+
+    # Ask for the user's preferred output path; if the output path is None, then
+    # the user has cancelled the operation
+    save_path = get_save_filename_input("Save Screenshot", "png", default_filename)
+    if save_path is None:
+        return
+
+    # Attempt to save the screenshot and show an error message if unsuccessful
+    if screenshot.save(save_path.decode("ascii")):
+        print(f"Screenshot saved to {save_path.decode('ascii')} successfully.")
+    else:
+        show_message_box(
+            "Error",
+            "Failed to save screenshot",
+            MessageBoxButtonSet.OKButtonSet,
+            MessageBoxIcon.ErrorIcon,
+        )
 
 
 def _save_view_image(scale: int) -> None:
@@ -86,6 +130,33 @@ def save_view_image_custom(_bv: BinaryView) -> None:
         _save_view_image(scale)
 
 
+def save_window_image_x1(_bv: BinaryView) -> None:
+    """
+    Shorthand function to save a screenshot of the main window at 1x resolution.
+    """
+
+    _save_window_image(1)
+
+
+def save_window_image_x2(_bv: BinaryView) -> None:
+    """
+    Shorthand function to save a screenshot of the main window at 2x resolution.
+    """
+
+    _save_window_image(2)
+
+
+def save_window_image_custom(_bv: BinaryView) -> None:
+    """
+    Shorthand to save a screenshot of the main window at a custom resolution.
+    """
+
+    scale = get_int_input("Resolution multiplier:", "Screenshot Ninja")
+
+    if scale is not None:
+        _save_window_image(scale)
+
+
 PluginCommand.register(
     "Screenshot Ninja \\ Save view image @ 1x...",
     "Save an image of the currently visible linear/graph view at 1x scaling",
@@ -102,4 +173,22 @@ PluginCommand.register(
     "Screenshot Ninja \\ Save view image...",
     "Save an image of the currently visible linear/graph view at custom scaling",
     save_view_image_custom,
+)
+
+PluginCommand.register(
+    "Screenshot Ninja \\ Save window image @ 1x...",
+    "Save an image of the main window at 1x scaling",
+    save_window_image_x1,
+)
+
+PluginCommand.register(
+    "Screenshot Ninja \\ Save window image @ 2x...",
+    "Save an image of the main window at 2x scaling",
+    save_window_image_x2,
+)
+
+PluginCommand.register(
+    "Screenshot Ninja \\ Save window image...",
+    "Save an image of the main window at custom scaling",
+    save_window_image_custom,
 )
